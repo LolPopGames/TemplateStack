3a056f54bbcaf235eb99f5f4a9ed21d81ac5e840
LolPopGames <157955292+LolPopGames@users.noreply.github.com>
2026-02-26 19:58:49 +0200

    feat: add struct macro

--- TemplateStack.h
+++ TemplateStack.h
@@ -1,16 +1,27 @@
 #ifndef _TEMPLATE_STACK_H_
 #define _TEMPLATE_STACK_H_
 
-/* Template Stack Prototype (for header) */
+/* ---- Includes ---- */
+#include <stddef.h>
+
+/* ---- Implementation macros ---- */
+#define _templatestack_struct(T) \
+    struct _templatestack_##T { \
+        T *buffer; \
+        size_t index; \
+        size_t size; \
+    }; \
+
+/* ---- Template Stack Prototype (for header) ---- */
 #define TemplateStack_proto(T)
 
-/* Template Stack Implementation (for source file) */
+/* ---- Template Stack Implementation (for source file) ---- */
 #define TemplateStack_impl(T)
 
-/* Template Stack Inline (all in one) */
+/* ---- Template Stack Inline (all in one) ---- */
 #define TemplateStack_inline(T)
 
-/* Template Stack Static (all functions are static) */
+/* ---- Template Stack Static (all functions are static) ---- */
 #define TemplateStack_static(T)
 
 #endif /* _TEMPLATE_STACK_H_ */
