86a33aa9683c698fe64013577a33b798b0d8926e
LolPopGames <157955292+LolPopGames@users.noreply.github.com>
2026-02-26 19:14:43 +0200

    chore: add all macros

--- TemplateStack.h
+++ TemplateStack.h
@@ -1,6 +1,16 @@
 #ifndef _TEMPLATE_STACK_H_
 #define _TEMPLATE_STACK_H_
 
+/* Template Stack Prototype (for header) */
+#define TemplateStack_proto(T)
 
+/* Template Stack Implementation (for source file) */
+#define TemplateStack_impl(T)
+
+/* Template Stack Inline (all in one) */
+#define TemplateStack_inline(T)
+
+/* Template Stack Static (all functions are static) */
+#define TemplateStack_static(T)
 
 #endif /* _TEMPLATE_STACK_H_ */
