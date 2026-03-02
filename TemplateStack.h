2cc767f01d1cca46e019719c2a50f8d1ee31073e
LolPopGames <lpg13.artiom@gmail.com>
2026-03-02 23:32:16 +0200

    chore: add license to header

--- TemplateStack.h
+++ TemplateStack.h
@@ -1,3 +1,30 @@
+/* SPDX-License-Identifier: MIT */
+/**
+ * @file TemplateStack.h
+ * @brief A single-header C library that implements a stack
+ * and allows it to be used with any type
+ *
+ * You can create multiple stacks for different types without
+ * writing separate stack implementations
+ *
+ * @author LolPopGames
+ * @date 2026
+ * @copyright
+ * Copyright © 2026 LolPopGames
+ *
+ * @license
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ */
 #ifndef _TEMPLATE_STACK_H_
 #define _TEMPLATE_STACK_H_
 
