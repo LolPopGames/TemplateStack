34f5ae39eafd3382a2a66923cee8fa9897eceaac
LolPopGames <lpg13.artiom@gmail.com>
2026-03-22 18:36:52 +0200

    feat: add module test

---
+++ tests/module/module.c
@@ -0,0 +1,14 @@
+/* Module */
+#include <stdio.h>
+#include "module.h"
+
+int
+main(void)
+{
+    Stack(int) stack = newStack(int)(1);
+    deleteStack(int)(&stack);
+
+    return 0;
+}
+
+TemplateStack_impl(int)
---
+++ tests/module/module.h
@@ -0,0 +1,8 @@
+#ifndef _MODULE_H_
+#define _MODULE_H_
+
+#include "TemplateStack.h"
+
+TemplateStack_proto(int)
+
+#endif /* _MODULE_H_ */
