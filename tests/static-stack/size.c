810998c088b91a7b0fa11cced609f738f608c6da
LolPopGames <157955292+LolPopGames@users.noreply.github.com>
2026-03-22 19:03:44 +0200

    feat: add static stack tests

---
+++ tests/static-stack/size.c
@@ -0,0 +1,39 @@
+/* Size */
+#include <stdio.h>
+#include "TemplateStack.h"
+
+StaticStack_inline(DOUBLE30, double, 30)
+
+int
+main(void)
+{
+    int i;
+    Stack(DOUBLE30) stack = {0};
+
+    if (stackSize(DOUBLE30)(&stack) != 0)
+        goto stackSize_error;
+
+    stackPush(DOUBLE30)(&stack, 43.22);
+
+    if (stackSize(DOUBLE30)(&stack) != 1)
+        goto stackSize_error;
+
+    stackPop(DOUBLE30)(&stack);
+
+    if (stackSize(DOUBLE30)(&stack) != 0)
+        goto stackSize_error;
+
+    for (i=0; i<30; i++)
+    {
+        stackPush(DOUBLE30)(&stack, i * 2.4582);
+    }
+
+    if (stackSize(DOUBLE30)(&stack) != 30)
+        goto stackSize_error;
+
+    return 0;
+
+stackSize_error:
+    fprintf(stderr, "stackSize does not work\n");
+    return 1;
+}
