5e2bd6e19fc6c2b6ba8d3956fb15ee508e88c7fa
LolPopGames <157955292+LolPopGames@users.noreply.github.com>
2026-04-16 10:22:18 +0300

    chore: remove underscore in _TEMPLATE_STACK_H_

--- tests/module/module.h
+++ tests/module/module.h
@@ -1,8 +1,8 @@
-#ifndef _MODULE_H_
-#define _MODULE_H_
+#ifndef _MODULE_H
+#define _MODULE_H
 
 #include "TemplateStack.h"
 
 TemplateStack_proto(int)
 
-#endif /* _MODULE_H_ */
+#endif /* _MODULE_H */
